b692d8008d08f3face3856454714025b6cac3a60
ggrz7 <9719836+ggrz7@users.noreply.github.com>
2024-02-07 07:39:54 +0100

    Add dummy layout private components

---
+++ src/app/components/index.ts
@@ -0,0 +1,2 @@
+export { default as MenuBar } from "./UI/MenuBar.tsx"
+export { default as SideBar } from "./UI/SideBar.tsx"
---
+++ src/app/containers/index.ts
@@ -0,0 +1,1 @@
+export { default as LayoutPrivate } from './LayoutPrivate'
