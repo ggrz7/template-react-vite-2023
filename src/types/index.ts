33ef40146b47275ca3f15d53444d239542fa787f
ggrz7 <9719836+ggrz7@users.noreply.github.com>
2024-03-06 08:08:31 +0100

    Add useful types

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,18 @@ declare type UserInfo = {
 	lastName?: string
 	role?: string
 }
+
+interface Error {
+	message: string;
+	response?: { data?: { message: string } }
+}
+
+interface SignInErrorConstructor {
+	new(message?: string): Error;
+	new(response?: { data?: { message: string } }): Error;
+	(message?: string): Error;
+	(response?: { data?: { message: string } }): Error;
+	readonly prototype: Error;
+}
+
+declare const SignInError: SignInErrorConstructor;
